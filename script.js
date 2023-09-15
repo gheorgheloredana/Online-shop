91c090aad6eb7ad4cf901da60f6045dbf9190697
Loredana Gheorghe <131867969+gheorgheloredana@users.noreply.github.com>
2023-09-15 13:50:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -94,51 +94,84 @@ closebutton.addEventListener("click", () => {
 
 let cart = document.querySelectorAll(".productpage");
 
-
-
 function onLoadCartNumbers() {
-    let productNumbers =localStorage.getItem("cartNumbers");
+  let productNumbers = localStorage.getItem("cartNumbers");
 
-    if(productNumbers) {
-        document.querySelector(".icontext span"). textContent = productNumbers;
-    }
+  if (productNumbers) {
+    document.querySelector(".icontext span").textContent = productNumbers;
+  }
 }
 
-function cartNumbers() {
-    let productNumbers = localStorage.getItem("cartNumbers");
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
 
-    productNumbers = parseInt(productNumbers);
-    if (productNumbers){
-        localStorage.setItem("cartNumbers", productNumbers + 1);
-        document.querySelector(".icontext span").textContent = productNumbers + 1;
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".icontext span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".icontext span").textContent = 1;
+  }
+
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems[product.tag] = product;
     }
-    else {
-        localStorage.setItem("cartNumbers", 1);
-        document.querySelector(".icontext span").textContent = 1;
-    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product
+    };
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+  let cartCost = localStorage.getItem("totalCost");
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + product.price);
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
 }
 
 let products = [
-    {
-        name: "Brielle Shimmer Dress - Wild Aster",
-        tag: "BrielleShimmerDress",
-        inCart: 0
-    },
+  {
+    name: "Brielle Shimmer Dress - Wild Aster",
+    tag: "BrielleShimmerDress",
+    price: 35,
+    inCart: 0
+  },
+  {
+    name: "Brielle Dress - Pink Grapefruit",
+    tag: "BrielleDress",
+    price: 25,
+    inCart: 0
+  }
+];
 
-    {
-        name: "Brielle Dress - Pink Grapefruit",
-        tag: "BrielleDress",
-        inCart: 0
-    }
-]
-
-for (let i=0; i < cart.length; i++) {
-cart[i].addEventListener("click", () => {
-    cartNumbers();
-})
+for (let i = 0; i < cart.length; i++) {
+  cart[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+  });
 }
-onLoadCartNumbers();
 
+
+onLoadCartNumbers();
+displayCart();
 
 //functie login form
 
